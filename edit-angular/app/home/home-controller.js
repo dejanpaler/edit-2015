45b4bf7be2355df09b090d741b5816dafa78f908
Gregor Menih <gmenih@gmail.com>
2015-06-30 15:38:05 +0200

    added home module

---
+++ edit-angular/app/home/home-controller.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc object
+   * @name home.controller:HomeCtrl
+   *
+   * @description
+   *
+   */
+  angular
+    .module('home')
+    .controller('HomeCtrl', HomeCtrl);
+
+  function HomeCtrl() {
+    var vm = this;
+    vm.ctrlName = 'HomeCtrl';
+  }
+}());

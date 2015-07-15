dc26e88347f5da4f28bd20db98a91d8613605de4
lukaza <luka.xbetas@gmail.com>
2015-07-15 11:06:58 +0200

    stash changes

--- edit-brick/src/main/java/com/ev3/BrickCommands.java
+++ edit-brick/src/main/java/com/ev3/BrickCommands.java
@@ -3,14 +3,7 @@ package com.ev3;
 import io.undertow.websockets.core.AbstractReceiveListener;
 import io.undertow.websockets.core.BufferedTextMessage;
 import io.undertow.websockets.core.WebSocketChannel;
-import io.undertow.websockets.core.WebSockets;
 import lejos.hardware.Button;
-
-import java.io.StringReader;
-
-import javax.json.Json;
-import javax.json.JsonException;
-import javax.json.JsonObject;
 
 public class BrickCommands extends AbstractReceiveListener {
 
@@ -19,38 +12,14 @@ public class BrickCommands extends AbstractReceiveListener {
     @Override
     protected void onFullTextMessage(WebSocketChannel channel,
             BufferedTextMessage message) {
-        String json = message.getData();
-        Log.info("Trying to parse JSON:" + json);
+        String msg = message.getData();
+        Log.info("Trying to parse msg:" + msg);
         this.channel = channel;
         try {
-            final JsonObject jsonCommand = Json.createReader(
-                    new StringReader(json)).readObject();
-            System.out.println("Parsed: " + jsonCommand.toString());
-            final String command = jsonCommand.getString("command");
-            WebSockets.sendText("[ev3.brick] Received command " + command,
-                    channel, null);
-            if (command.isEmpty())
-                Log.info("No command given.");
-            else if (command.equals("MoveToLocation")) {
-                MoveToLocation();
-            } else if (command.equals("MoveHome")) {
-                MoveHome();
-            } else if (command.equals("PickUpItem")) {
-                PickupItem();
-            } else if (command.equals("DropItem")) {
-                DropItem();
-            } else {
-                Log.info("Command " + command + " doesn't exist.");
-            }
-            if (command.equals("Stop")) {
-                Log.info("Press escape to exit.");
-                if (Button.waitForAnyPress() == Button.ID_ESCAPE) {
-                    System.exit(0);
-                }
-            }
-        } catch (JsonException je) {
-            System.out.print("Couldn't parse JSON.");
-            System.out.print(je.getMessage());
+            System.out.println(msg);
+        } catch ( Exception e) {
+            System.out.print("Couldn't parse Txt.");
+            System.out.print(e.getMessage());
         }
     }
 

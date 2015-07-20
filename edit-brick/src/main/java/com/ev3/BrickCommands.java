55bd35e7f58db861c9552b176a6941df50bbddcb
Rasencove <rabbit005@gmail.com>
2015-07-20 09:15:51 +0200

    kr neka

--- edit-brick/src/main/java/com/ev3/BrickCommands.java
+++ edit-brick/src/main/java/com/ev3/BrickCommands.java
@@ -13,7 +13,6 @@ import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
@@ -36,8 +35,6 @@ public class BrickCommands extends AbstractReceiveListener {
     private static EV3ColorSensor colorSensor = new EV3ColorSensor(colorSensorPort);
     private static RegulatedMotor lm = new EV3LargeRegulatedMotor(MotorPort.B);
     private static RegulatedMotor rm = new EV3LargeRegulatedMotor(MotorPort.C);
-    private static RegulatedMotor gripper = new EV3MediumRegulatedMotor(MotorPort.A);
-    private static EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
 
     @Override
     protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
@@ -70,7 +67,7 @@ public class BrickCommands extends AbstractReceiveListener {
                     break;
 
                 case "drop":
-                    dropItem();
+                    DropItem();
                     break;
 
                 case "turn right":
@@ -101,8 +98,8 @@ public class BrickCommands extends AbstractReceiveListener {
         }
     }
 
-    
-    
+
+
     private Order ParseCommand(String message) {
         int i = message.indexOf(";");
         int j = message.indexOf(";", i+1);
@@ -112,146 +109,108 @@ public class BrickCommands extends AbstractReceiveListener {
         order.side = Integer.parseInt(message.substring(j+1));
         return order;
     }
-    
-    public static void find_path(int x, int y, boolean go_left, boolean take_from) 
+
+    private static void find_path(int x, int y, boolean go_left, boolean take_from)
     {
-        
+
         lm.setSpeed(420);
         rm.setSpeed(420);
 
         int current_x = 0;
-        int current_y = 1;
+        int current_y = 0;
         int colorID;
 
         int WHITE = 6;
         int BLACK = 7;
         int RED = 0;
-        
+
         /*
          * === COLORS ===
          * 6 - WHITE
          * 7 - BLACK/VERY DARK SOMETHING
          * 0 - RED
          */
-        
-        // GO TO OBJECT
-        while (true) 
-        {
-            colorID = colorSensor.getColorID(); // do a color reading
 
-            if (colorID == WHITE) // need to find the line again
-            {
-                find_line_again();
-            }
-            else if (colorID == 5 || colorID == RED) // if we've found an intersection (red color)    
-            {
-                System.out.println("found an intersection!");
-                if (current_y != y) // if we still need to go up
-                {
-                    System.out.println("goin fwd");
-                    current_y++;
-                    ForwardIntersection();
-                }
-                else if(current_y == y && current_x == 0) // if we've found the intersection on which we need to turn
-                {
-                    if (x < 0)
-                    {
-                        System.out.println("goin left");
-                        current_x--;
-                        rotateL();
-                    }
-                    else
-                    {
-                        System.out.println("goin right");
-                        current_x++;
-                        rotateR();
-                    }
-                }
-                else if(current_y == y && current_x == x) // if we're on the final intersection
-                {
-                    if (go_left)
-                    {
-                        System.out.println("final left turn");
-                        rotateR();
-                    }
-                    else
-                    {
-                        System.out.println("final right turn");
-                        rotateL();
-                    }
-                    Sound.beepSequence();
-                    objectGrabbingProcedure();
-                    rotateBackOnLine(!go_left);
-                    break;
-                }
-                else // or we're moving horizontally (fwd on the X axis)
-                {
-                    System.out.println("goin fwd (sideways)");
-                    ForwardIntersection();
-                    if (x < 0)
-                        current_x--;
-                    else
-                        current_x++;
-                }
-            }
-            else // base case is, that we're on the line, so go forward - fwd
-            {
-                System.out.println("fwd");
-                lm.backward();
-                rm.backward();
-            }
-        }
-        
-        // GO BACK TO START POINT
-        
-        current_x = x < 0 ? x + 1 : x - 1;
-        current_y = y;
         while (true)
         {
             colorID = colorSensor.getColorID(); // do a color reading
 
             if (colorID == WHITE) // need to find the line again
             {
-                find_line_again();
+                // turn left and try to find the line again
+                System.out.println("find line left");
+                lm.stop();
+                rm.stop();
+                boolean found = false;
+                rm.rotate(-100, true);
+                lm.rotate(100, true);
+
+                while (rm.isMoving() && lm.isMoving()) {
+                    // sample = getSample();
+                    colorID = colorSensor.getColorID();
+                    if (colorID == 7) // found it!
+                    {
+                        rm.stop(true);
+                        lm.stop();
+                        found = true;
+                        break;
+                    }
+                }
+
+                // if searching towards left didn't succeed, search towards right
+                if (!found) {
+                    System.out.println("find line right");
+                    lm.rotate(-200, true);
+                    rm.rotate(200, true);
+
+                    while (rm.isMoving() && lm.isMoving()) {
+                        // sample = getSample();
+                        colorID = colorSensor.getColorID();
+                        if (colorID == 7)  // found it!
+                        {
+                            lm.stop(true);
+                            rm.stop();
+                            found = true;
+                            break;
+                        }
+                    }
+                }
             }
-            else if (colorID == 5 || colorID == RED) // if we've found an intersection (red color)    
+            else if (colorID == 5 || colorID == RED) // if we've found an intersection (red color)
             {
-                
                 System.out.println("found an intersection!");
-                if (current_x != 0)
+                if ((current_y - Math.abs(current_x) < y - 1) && current_x != x) // then march on!
                 {
                     System.out.println("goin fwd");
+                    current_y++;
                     ForwardIntersection();
-                    // update current_x position
-                    if (x < 0)
-                        current_x++;
-                    else
-                        current_x--;
                 }
-                else if(current_x == 0 && current_y == y) // if we've hit the only intersection on which we have to turn
+                else if (current_x == x && go_left) // then make the final turn and end the loop
                 {
-                    if (x < 0)
-                        rotateR();
-                    else
-                        rotateL();
-                    current_y--;
-                }
-                else if(current_y != 0)
-                {
-                    System.out.println("goin fwd");
-                    ForwardIntersection();
-                    current_y--;
-                }
-                else
-                {
-                    // DROP ITEM AND TURN AROUND
+                    System.out.println("final left turn");
+                    rotateR();
                     Sound.beepSequence();
-                    rm.stop(true);
-                    lm.stop();
-                    dropItem();
-                    rotate180();
-                    //ForwardIntersection();
                     break;
+                }
+                else if (current_x == x && !go_left) // then make the final turn and end the loop
+                {
+                    System.out.println("final right turn");
+                    rotateL();
+                    Sound.beepSequence();
+                    break;
+                }
+                else if (x < 0) // then turn left
+                {
+                    System.out.println("goin left");
+                    current_x--;
+                    rotateL();
+
+                }
+                else if (x > 0) // then turn right
+                {
+                    System.out.println("goin right");
+                    current_x++;
+                    rotateR();
                 }
             }
             else // base case is, that we're on the line, so go forward - fwd
@@ -261,54 +220,9 @@ public class BrickCommands extends AbstractReceiveListener {
                 rm.backward();
             }
         }
-        
-        
     }
 
-    private static void find_line_again()
-    {
-     // turn left and try to find the line again
-        System.out.println("find line left");
-        lm.stop(true);
-        rm.stop();
-        boolean found = false;
-        rm.rotate(-100, true);
-        lm.rotate(100, true);
-        int colorID;
-        
-        while (rm.isMoving() && lm.isMoving()) {
-            // sample = getSample();
-            colorID = colorSensor.getColorID();
-            if (colorID == 7) // found it!
-            {
-                rm.stop(true);
-                lm.stop();
-                found = true;
-                break;
-            }
-        }
-
-        // if searching towards left didn't succeed, search towards right
-        if (!found) {
-            System.out.println("find line right");
-            lm.rotate(-200, true);
-            rm.rotate(200, true);
-
-            while (rm.isMoving() && lm.isMoving()) {
-                // sample = getSample();
-                colorID = colorSensor.getColorID();
-                if (colorID == 7)  // found it!
-                {
-                    lm.stop(true);
-                    rm.stop();
-                    found = true;
-                    break;
-                }
-            }
-        }
-    }
-    
-    private static void rotateL() 
+    private static void rotateL()
     {
         lm.stop(true);
         rm.stop(true);
@@ -316,7 +230,7 @@ public class BrickCommands extends AbstractReceiveListener {
         lm.rotate(-620);
     }
 
-    private static void rotateR() 
+    private static void rotateR()
     {
         lm.stop(true);
         rm.stop(true);
@@ -324,69 +238,23 @@ public class BrickCommands extends AbstractReceiveListener {
         rm.rotate(-620);
     }
 
-    private static void rotate180()
-    {
-        lm.rotate(840, true);
-        rm.rotate(840);
-    }
-    
-    private static void ForwardIntersection() 
+    private static void ForwardIntersection()
     {
         lm.stop(true);
         rm.stop();
-        lm.rotate(-200, true);
-        rm.rotate(-200);
+        lm.rotate(-800, true);
+        rm.rotate(-800);
     }
 
-    private static void objectGrabbingProcedure()
-    {
-        lm.stop(true);
-        rm.stop();
-        lm.setSpeed(100);
-        rm.setSpeed(100);
-        
-        lm.forward();
-        rm.forward();
-        
-        while (true) 
-        {
-            int sampleSize = touchSensor.sampleSize();
-            float[] sample = new float[sampleSize];
-            touchSensor.fetchSample(sample, 0);
-            if(sample[0] == 1)
-            {
-                lm.stop(true);
-                rm.stop();
-                grabItem();
-                break;
-            }
-        }
-        
-        lm.stop(true);
-        rm.stop();
-        lm.setSpeed(420);
-        rm.setSpeed(420);
-    }
-    
-    public static void grabItem()
-    {
-        gripper.rotate(180);
-        Delay.msDelay(1000);
-        gripper.forward();
-        Delay.msDelay(1000);
-    }
-    
-    
-    private void PickupItem() 
-    {
+    private void PickupItem() {
         Log.info("Picking up item");
         //move forward
 
-        gripper.rotate(-180);
+        Motor.A.rotate(-180);
         Delay.msDelay(1000);
-        gripper.rotate(180);
+        Motor.A.rotate(180);
         Delay.msDelay(1000);
-        gripper.forward();
+        Motor.A.forward();
         Delay.msDelay(1000);
 
         //move backward
@@ -394,20 +262,25 @@ public class BrickCommands extends AbstractReceiveListener {
         Log.info("Item picked up");
     }
 
-    private static void dropItem() {
+    private void DropItem() {
         Log.info("Dropping..:");
-        gripper.backward();
+        //move forward
+
+        Motor.A.backward();
         Delay.msDelay(1000);
-        gripper.rotate(-180);
+        Motor.A.rotate(-180);
+        Motor.B.setSpeed(720);// 2 RPM 720
+        Motor.C.setSpeed(720);
+        Motor.B.backward();
+        Motor.C.backward();
+
+        //move backward
+        Button.LEDPattern(3);
         Log.info("Item dropped.");
     }
 
-    private static void rotateBackOnLine(boolean turn_left)
-    {
-        if(turn_left)
-            lm.rotate(-840); //rotiraj 90° levo
-        else
-            rm.rotate(-840); //rotiraj 90° Desno 
+    private void MoveToNextIntersection(){
+        //move until next yellow dot
     }
 
     private void Turn(String direction){
@@ -426,8 +299,7 @@ public class BrickCommands extends AbstractReceiveListener {
         System.out.print("Press Back, when you want to stop!");
         Motor.B.forward();
         Motor.C.forward();
-        while(true)
-        {
+        while(true){
             if (Button.waitForAnyPress() == Button.ID_ESCAPE) {
                 Motor.B.stop();
                 Motor.C.stop();

c207a96471f463c7905f4ef1d296bf4714f07870
unknown <kokanmalenko69@gmail.com>
2015-07-20 15:56:41 +0200

    Canvas map and urls

--- edit-angular/app/item/add-item-controller.js
+++ edit-angular/app/item/add-item-controller.js
@@ -27,23 +27,11 @@
     });
 
     vm.addItem = function (title) {
-      $http.post("http://10.80.49.2:8080/edit-javaee/items/do", {"id": title, "command": "get"})
-        .success(function(){
-          alert("Success");
-        })
-        .error(function(){
-          alert("Error");
-        });
+      $http.post('http://10.80.49.2:8080/edit-javaee/items/put', {title: title});
     };
 
-    vm.removeItem = function (id) {
-      $http.post("http://10.80.49.2:8080/edit-javaee/items/do", {"id": id, "command": "get"})
-        .success(function(){
-          alert("Success");
-        })
-        .error(function(){
-          alert("Error");
-        });
+    vm.editItem = function (id, title) {
+      $http.post('http://10.80.49.2:8080/edit-javaee/items/edit', {id: id, title: title});
     };
   }
 }());

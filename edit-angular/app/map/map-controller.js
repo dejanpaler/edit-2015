bcafbad6d9b016f7fe4b16719c5d596f85740e85
unknown <kokanmalenko69@gmail.com>
2015-07-22 15:58:46 +0200

    Almost over

--- edit-angular/app/map/map-controller.js
+++ edit-angular/app/map/map-controller.js
@@ -12,12 +12,16 @@
     .module('map')
     .controller('MapCtrl', MapCtrl);
 
-  function MapCtrl() {
+  function MapCtrl($websocket) {
     var i,
       j,
       vm = this,
-      posY = 10,
+      socket,
+      commands,
+      posY = 8,
       posX = 5,
+      corX = 0,
+      corY = 0,
       tile = 32,
       canvas = document.getElementById('canvas'),
       context = canvas.getContext('2d'),
@@ -29,7 +33,7 @@
       pick = 2,
       mapArray = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 3, 0, 0, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
@@ -43,64 +47,95 @@
     vm.ctrlName = 'MapCtrl';
 
     grass.src = '/images/grass.png';
-    produk.src = '/images/searched.png';
+    // produk.src = '/images/searched.png';
     robot.src = '/images/dogeRight.png';
     glavniot.src = '/images/product.png';
     fateno.src = '/images/dogePacketRight.png';
 
-    vm.drawMap2 = function () {
-      console.log(robot.src);
-      for (i = 0; i < mapArray.length; i++) {
-        for (j = 0; j < mapArray[i].length; j++) {
-          context.drawImage(grass, (j * tile), (i * tile), tile, tile);
-          if (mapArray[i][j] === 1) {
-            context.drawImage(glavniot, (j * tile), (i * tile), tile, tile);
-          }
-          if (mapArray[i][j] === 2) {
-            context.drawImage(robot, (j * tile), (i * tile), tile, tile);
-          }
-          if (mapArray[i][j] === 3) {
-            context.drawImage(produk, (j * tile), (i * tile), tile, tile);
-          }
-          if (mapArray[i][j] === 4) {
-            context.drawImage(fateno, (j * tile), (i * tile), tile, tile);
+    mapArray[posY][posX] = 2;
+
+    vm.listen = function (msg) {
+      try {
+        commands = angular.fromJson(msg.data);
+        if (commands.coordinate) {
+          if (parseInt(commands.coordinate.split(',')[0], 10) - corY === 1) {
+            setTimeout(function () {
+              vm.up();
+            }, 500);
+            setTimeout(function () {
+              vm.up();
+            }, 1000);
+            setTimeout(function () {
+              vm.up();
+            }, 1500);
+            corY += 1;
+            alert(corY);
+          } else if (parseInt(commands.coordinate.split(',')[0], 10) - corY === -1) {
+            setTimeout(function () {
+              vm.down();
+            }, 500);
+            setTimeout(function () {
+              vm.down();
+            }, 1000);
+            setTimeout(function () {
+              vm.down();
+            }, 1500);
+            corY -= 1;
+            alert(corY);
+          } else if (parseInt(commands.coordinate.split(',')[1], 10) - corX === -1) {
+            setTimeout(function () {
+              vm.left();
+            }, 500);
+            setTimeout(function () {
+              vm.left();
+            }, 1000);
+            corX -= 1;
+            alert(corX);
+          } else if (parseInt(commands.coordinate.split(',')[1], 10) - corX === 1) {
+            setTimeout(function () {
+              vm.right();
+            }, 500);
+            setTimeout(function () {
+              vm.right();
+            }, 1000);
+            corX += 1;
+            alert(corX);
           }
         }
+      } catch(err) {
+        console.log("Error: %s", err);
+      } finally {
+        console.log(msg);
       }
     };
 
-    mapArray[posY][posX] = 2;
+    init();
 
-    vm.drawMap = function () {
-      angular.element('#mapa').append('<div id="container"></div>');
-      for (i = 0; i < mapArray.length; i++) {
-        for (j = 0; j < mapArray[i].length; j++) {
-          if (mapArray[i][j] === 0) {
-            angular.element('#container').append('<div class="floor"></div>');
-          }
-          if (mapArray[i][j] === 1) {
-            angular.element('#container').append('<div class="items"></div>');
-          }
-          if (mapArray[i][j] === 2) {
-            angular.element('#container').append('<div class="robot"></div>');
-          }
-          if (mapArray[i][j] === 3) {
-            angular.element('#container').append('<div class="item"></div>');
-          }
-          if (mapArray[i][j] === 4) {
-            angular.element('#container').append('<div class="picked"></div>');
+    vm.drawMap2 = function () {
+      setTimeout(function () {
+        for (i = 0; i < mapArray.length; i++) {
+          for (j = 0; j < mapArray[i].length; j++) {
+            context.drawImage(grass, (j * tile), (i * tile));
+            if (mapArray[i][j] === 1) {
+              context.drawImage(glavniot, (j * tile), (i * tile));
+            }
+            if (mapArray[i][j] === 2) {
+              context.drawImage(robot, (j * tile), (i * tile));
+            }
+            if (mapArray[i][j] === 4) {
+              context.drawImage(fateno, (j * tile), (i * tile));
+            }
           }
         }
-      }
+      }, 20);
     };
 
     vm.up = function () {
-      angular.element('#container').remove();
       mapArray[posY][posX] = 0;
       if (posY > 0) {
         posY -= 1;
       }
-      if (mapArray[posY][posX] === 3) {
+      if (mapArray[posY][posX] === 1) {
         pick = 4;
       }
       if (posY === 10 && posX === 5) {
@@ -108,17 +143,15 @@
       }
 
       mapArray[posY][posX] = pick;
-      vm.drawMap();
       vm.drawMap2();
     };
 
     vm.down = function () {
-      angular.element('#container').remove();
       mapArray[posY][posX] = 0;
       if (posY < 10) {
         posY += 1;
       }
-      if (mapArray[posY][posX] === 3) {
+      if (mapArray[posY][posX] === 1) {
         pick = 4;
       }
       if (posY === 10 && posX === 5) {
@@ -126,17 +159,15 @@
       }
 
       mapArray[posY][posX] = pick;
-      vm.drawMap();
       vm.drawMap2();
     };
 
     vm.left = function () {
-      angular.element('#container').remove();
       mapArray[posY][posX] = 0;
       if (posX > 0) {
         posX -= 1;
       }
-      if (mapArray[posY][posX] === 3) {
+      if (mapArray[posY][posX] === 1) {
         pick = 4;
       }
       if (posY === 10 && posX === 5) {
@@ -146,17 +177,15 @@
       mapArray[posY][posX] = pick;
       robot.src = '/images/dogeLeft.png';
       fateno.src = '/images/dogePacketLeft.png';
-      vm.drawMap();
       vm.drawMap2();
     };
 
     vm.right = function () {
-      angular.element('#container').remove();
       mapArray[posY][posX] = 0;
       if (posX < 10) {
         posX += 1;
       }
-      if (mapArray[posY][posX] === 3) {
+      if (mapArray[posY][posX] === 1) {
         pick = 4;
       }
       if (posY === 10 && posX === 5) {
@@ -166,8 +195,19 @@
       mapArray[posY][posX] = pick;
       robot.src = '/images/dogeRight.png';
       fateno.src = '/images/dogePacketRight.png';
-      vm.drawMap();
       vm.drawMap2();
     };
+
+    function init() {
+      socket = $websocket('ws://10.80.49.2:8080/edit-javaee/angular');
+
+      socket.onMessage(function (msg) {
+        vm.listen(msg);
+      });
+
+      socket.onOpen(function () {
+        console.log('Connection opened');
+      });
+    }
   }
 }());
